5ab11abc129494dda0cd249e2458b46db1064208
lindavataksi <lindavataksi3@gmail.com>
2024-05-27 21:55:12 -0400

    added visualization plots to front-end

--- StockPredictor-project/app.py
+++ StockPredictor-project/app.py
@@ -14,14 +14,14 @@ def predict():
         current_price, future_price, accuracy_percentage = predict_stock_price(ticker)
         prediction_text = f"The current price of {ticker} is ${current_price} and the predicted price after 1 year is ${future_price}."
         accuracy = f"Accuracy : {accuracy_percentage}%"
-        return render_template('index.html', prediction_text=prediction_text, accuracy = accuracy)
-    elif request.method == 'GET':
+    # elif request.method == 'GET':
         image_urls = {
             'image1': url_for('static', filename='plot1.png'),
             'image2': url_for('static', filename='plot2.png'),
             'image3': url_for('static', filename='plot3.png')
         }
-        return render_template('index.html', image_urls = image_urls)
+        return render_template('index.html', prediction_text=prediction_text, accuracy = accuracy, image_urls = image_urls)
+        # return render_template('index.html', image_urls = image_urls)
     return render_template('index.html')
 
 
